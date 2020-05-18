63a07f0c920802b04eb3bdbf6cef38f12422e41e
jadugnap <jadugnap@gmail.com>
2020-05-19 03:25:42 +0800

    Configure KSQL to aggregate COUNT(turnstile) for each station.

--- producers/models/turnstile.py
+++ producers/models/turnstile.py
@@ -30,7 +30,7 @@ class Turnstile(Producer):
         # TODO: Complete the below by deciding on a topic name, number of partitions, and number of
         # replicas
         super().__init__(
-            f"{station_name}.turnstile.event", # TODO: Come up with a better topic name
+            f"dashboard.producer.turnstile", # TODO: Come up with a better topic name
             key_schema=Turnstile.key_schema,
             value_schema=Turnstile.value_schema,
             num_partitions=1, # to revisit if necessary
